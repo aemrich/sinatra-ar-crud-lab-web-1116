b5f0ebdb37da1d52fd14ce1c5d569ef09277c278
aemrich <adam.emrich@gmail.com>
2016-12-21 13:59:14 -0500

    Rack::MethodOverride FTW

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,45 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do 
-    
+  get '/posts/new' do
+    erb :new
   end
+
+  get '/posts' do
+    @posts = Post.all
+    erb :posts
+  end
+
+  post '/posts' do
+    Post.create(name: params[:name], content: params[:content])
+
+    redirect to ('/posts')
+  end
+
+  get '/posts/:id/edit' do
+    @post = Post.find(params[:id])
+    erb :edit
+  end
+
+  get '/posts/:id' do
+    @post = Post.find(params[:id])
+    erb :'show'
+  end
+
+
+  patch '/posts/:id' do
+    @post = Post.find(params[:id])
+    @post.update(name: params[:name], content: params[:content])
+    redirect "/posts/#{@post.id}"
+  end
+
+  delete '/posts/:id/delete' do
+    @post = Post.find(params[:id])
+    @post.delete
+    erb :deleted
+  end
+
+
+
+
 end
